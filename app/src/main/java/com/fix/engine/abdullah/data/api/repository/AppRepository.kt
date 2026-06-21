744e3863d8b8fd91732b25664bf4f681a1058a56
abdullah14120 <abdullah.tamemii@gmail.com>
2026-06-21 17:37:30 +0300

    Update AppRepository.kt

--- app/src/main/java/com/fix/engine/abdullah/data/api/repository/AppRepository.kt
+++ app/src/main/java/com/fix/engine/abdullah/data/api/repository/AppRepository.kt
@@ -54,10 +54,6 @@ class AppRepository {
      * المحرك الداخلي السري لفك بوابات الـ XOR لضمان تشويه الرابط تماماً
      * في حال محاولة سحب الـ Heap Dump من الذاكرة
      */
-    /**
-     * المحرك الداخلي السري لفك بوابات الـ XOR لضمان تشويه الرابط تماماً
-     * في حال محاولة سحب الـ Heap Dump من الذاكرة
-     */
     private fun decryptUrlStream(secureBytes: ByteArray, salt: Byte): String {
         val output = ByteArray(secureBytes.size)
         for (i in secureBytes.indices) {
@@ -65,4 +61,4 @@ class AppRepository {
         }
         return String(output, Charsets.UTF_8)
     }
-
+}
